8ef0bbb2903d51a2a3bc2e209bb54e919a4b0bfb
René Brummel <rbr@redandbundle.com>
2025-09-22 15:53:18 +0200

    add nld translations

--- Scripts/Translate.ps1
+++ Scripts/Translate.ps1
@@ -3,15 +3,15 @@ Import-Module 'C:\Program Files\Reports ForNAV\ForNav.Cmdlet.dll'
 # Set-Location -Path $PSScriptRoot
 
 Invoke-ExportTranslationFromXlfToExcel `
-    -FromXlf '.\Translations\'`
+    -FromXlf '.\Red Regenerator\Translations\'`
     -ToExcel '.\Scripts\Translations-Generated.xlsx'
 
 Invoke-ImportTranslationFromExcelToXlf `
-    -FromXlf '.\Translations\Red Regenerator.g.xlf'`
-    -FromExcel '.\Scripts\Translations - baseline.xlsx'`
-    -ToXlf '.\Translations'
+    -FromXlf '.\Red Regenerator\Translations\Red Regenerator.g.xlf'`
+    -FromExcel '.\Red Regenerator\Scripts\Translations - baseline.xlsx'`
+    -ToXlf '.\Red Regenerator\Translations'
 
 Invoke-ImportTranslationFromExcelToXlf `
-    -FromXlf '.\Translations\Red Ink Text Templates.g.xlf'`
+    -FromXlf '.\Red Regenerator\Translations\Red Regenerator.g.xlf'`
     -FromExcel '.\Scripts\Translations.xlsx'`
-    -ToXlf '.\Translations'
+    -ToXlf '.\Red Regenerator\Translations'
